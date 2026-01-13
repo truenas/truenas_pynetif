4ef1ba77d5950db406d15f4d50d81c2433cd2c45
caleb <yocalebo@gmail.com>
2026-01-13 09:35:32 -0500

    add netlink address module

--- truenas_pynetif/address/mixin.py
+++ truenas_pynetif/address/mixin.py
@@ -1,81 +1,65 @@
-import ipaddress
+import socket
 import time
 
-from pyroute2 import IPRoute
-from pyroute2.netlink.exceptions import NetlinkDumpInterrupted
-
-from .ipv6 import ipv6_netmask_to_prefixlen
-from .types import AddressFamily, InterfaceAddress, LinkAddress
+from .constants import AddressFamily
+from .netlink import AddressInfo, DumpInterrupted, get_address_netlink
 from ..utils import run
 
-
-__all__ = ["AddressMixin"]
+__all__ = ("AddressMixin",)
 
 
 class AddressMixin:
-    def add_address(self, address):
+    def add_address(self, address: AddressInfo):
         self._address_op("add", address)
 
     def flush(self):
         # Remove all configured ip addresses
-        run(['ip', 'addr', 'flush', 'dev', self.name, 'scope', 'global'])
+        run(["ip", "addr", "flush", "dev", self.name, "scope", "global"])
 
-    def remove_address(self, address):
+    def remove_address(self, address: AddressInfo):
         self._address_op("del", address)
 
-    def replace_address(self, address):
+    def replace_address(self, address: AddressInfo):
         self._address_op("replace", address)
 
-    def _address_op(self, op, address):
-        if isinstance(address.address, LinkAddress):
-            return
+    def _address_op(self, op: str, address: AddressInfo):
+        if address.family == AddressFamily.INET:
+            cmd = ["ip", "addr", op, f"{address.address}/{address.prefixlen}"]
+            if op == "add":
+                cmd.extend(["brd", "+"])
+            cmd.extend(["dev", self.name])
+            run(cmd)
+        elif address.family == AddressFamily.INET6:
+            cmd = [
+                "ip",
+                "addr",
+                op,
+                f"{address.address}/{address.prefixlen}",
+                "dev",
+                self.name,
+            ]
+            run(cmd)
 
-        netmask = str(address.netmask)
-        if isinstance(address.address, ipaddress.IPv6Address):
-            netmask = ipv6_netmask_to_prefixlen(netmask)
+    def _get_addresses(self) -> list[AddressInfo]:
+        try:
+            if_index = socket.if_nametoindex(self.name)
+        except OSError:
+            return []
 
-        cmd = ["ip", "addr", op, f"{address.address}/{netmask}"]
-        if op == 'add':
-            # make sure we tell linux to assign proper broadcast address
-            # when adding an IPv4 address to an interface
-            # (doesn't apply to IPv6)
-            cmd.extend(["brd", "+"]) if ':' not in f'{address.address}' else None
-        cmd.extend(["dev", self.name])
-
-        run(cmd)
-
-    def _get_addresses(self):
-        addresses = []
-        with IPRoute(strict_check=True) as ipr:
-            # strict_check forces kernel to do the filtering increasing performance
-            if index := (ipr.link_lookup(ifname=self.name) or [None])[0]:
-                # The kernel doesn't return IFA_LABEL for IPv6 addresses so we have
-                # to lookup the index for a given interface
-                for addr in ipr.addr('dump', index=index):
-                    if addr['family'] == AddressFamily.INET.value:
-                        addresses.append(InterfaceAddress(
-                            AddressFamily.INET,
-                            ipaddress.IPv4Interface(f'{addr.get_attr("IFA_ADDRESS")}/{addr["prefixlen"]}'),
-                        ))
-                    elif addr['family'] == AddressFamily.INET6.value:
-                        addresses.append(InterfaceAddress(
-                            AddressFamily.INET6,
-                            ipaddress.IPv6Interface(f'{addr.get_attr("IFA_ADDRESS")}/{addr["prefixlen"]}'),
-                        ))
-
-                for mac in ipr.link('dump', index=index):
-                    if mac_addr := mac.get_attr('IFLA_ADDRESS'):
-                        addresses.append(InterfaceAddress(AddressFamily.LINK, LinkAddress(self.name, mac_addr)))
-
-        return addresses
+        return [
+            addr
+            for addr in get_address_netlink().get_addresses()
+            if addr.index == if_index
+            and addr.family in (AddressFamily.INET, AddressFamily.INET6)
+        ]
 
     @property
-    def addresses(self):
+    def addresses(self) -> list[AddressInfo]:
         retries = 5
         while True:
             try:
                 return self._get_addresses()
-            except NetlinkDumpInterrupted:
+            except DumpInterrupted:
                 # low-grade hardware can produce this which
                 # isn't necessarily fatal and the request
                 # should be retried
---
+++ truenas_pynetif/address/netlink.py
@@ -0,0 +1,323 @@
+import socket
+import struct
+from contextvars import ContextVar
+from dataclasses import dataclass, field
+
+from .constants import (
+    NETLINK_ROUTE,
+    NLA_F_NESTED,
+    AddressFamily,
+    IFAAttr,
+    IFLAAttr,
+    NLMsgFlags,
+    NLMsgType,
+    RTEXTFilter,
+    RTMType,
+)
+from ..exceptions import (
+    DeviceNotFound,
+    DumpInterrupted,
+    NetlinkError,
+    OperationNotSupported,
+)
+
+__all__ = (
+    "AddressInfo",
+    "AddressNetlink",
+    "DeviceNotFound",
+    "DumpInterrupted",
+    "LinkInfo",
+    "NetlinkError",
+    "OperationNotSupported",
+    "close_address_netlink",
+    "get_address_netlink",
+)
+
+_address_ctx: ContextVar["AddressNetlink | None"] = ContextVar(
+    "address_netlink", default=None
+)
+
+
+@dataclass(slots=True, frozen=True, kw_only=True)
+class AddressInfo:
+    family: int
+    prefixlen: int
+    flags: int
+    scope: int
+    index: int
+    address: str
+    local: str | None
+    broadcast: str | None
+    label: str | None
+
+
+@dataclass(slots=True, frozen=True, kw_only=True)
+class LinkInfo:
+    index: int
+    flags: int
+    mtu: int
+    operstate: int
+    address: str | None
+    perm_address: str | None
+
+
+@dataclass(slots=True)
+class AddressNetlink:
+    _sock: socket.socket | None = field(default=None, init=False)
+    _seq: int = field(default=0, init=False)
+    _pid: int | None = field(default=None, init=False)
+
+    def __enter__(self):
+        self._connect()
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def _connect(self):
+        self._sock = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, NETLINK_ROUTE)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1048576)
+        self._sock.bind((0, 0))
+        self._pid = self._sock.getsockname()[0]
+
+    def close(self):
+        if self._sock:
+            self._sock.close()
+            self._sock = None
+
+    def _pack_nlattr(self, attr_type: int, data: bytes) -> bytes:
+        nla_len = 4 + len(data)
+        padded_len = (nla_len + 3) & ~3
+        padding = padded_len - nla_len
+        return struct.pack("HH", nla_len, attr_type) + data + b"\x00" * padding
+
+    def _pack_nlattr_str(self, attr_type: int, s: str) -> bytes:
+        return self._pack_nlattr(attr_type, s.encode() + b"\x00")
+
+    def _pack_nlattr_u32(self, attr_type: int, val: int) -> bytes:
+        return self._pack_nlattr(attr_type, struct.pack("I", val))
+
+    def _pack_nlattr_nested(self, attr_type: int, attrs: bytes) -> bytes:
+        return self._pack_nlattr(attr_type | NLA_F_NESTED, attrs)
+
+    def _next_seq(self) -> int:
+        self._seq += 1
+        return self._seq
+
+    def _pack_nlmsg(self, msg_type: int, flags: int, payload: bytes) -> bytes:
+        seq = self._next_seq()
+        nlmsg_len = 16 + len(payload)
+        return struct.pack("IHHII", nlmsg_len, msg_type, flags, seq, 0) + payload
+
+    def _recv_msgs(self) -> list[tuple[int, bytes]]:
+        messages = []
+        while True:
+            data = self._sock.recv(65536)
+            offset = 0
+            done = False
+            while offset < len(data):
+                if offset + 16 > len(data):
+                    break
+                nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid = (
+                    struct.unpack_from("IHHII", data, offset)
+                )
+                if nlmsg_len < 16:
+                    break
+                if nlmsg_flags & NLMsgFlags.DUMP_INTR:
+                    raise DumpInterrupted("Netlink dump was interrupted")
+                if nlmsg_type == NLMsgType.ERROR:
+                    if offset + 20 <= len(data):
+                        error = struct.unpack_from("i", data, offset + 16)[0]
+                        if error < 0:
+                            error = -error
+                            if error == 19:  # ENODEV
+                                raise DeviceNotFound("No such device")
+                            elif error == 95:  # EOPNOTSUPP
+                                raise OperationNotSupported("Operation not supported")
+                            raise NetlinkError(f"Netlink error: {error}")
+                    done = True
+                elif nlmsg_type == NLMsgType.DONE:
+                    done = True
+                else:
+                    payload = data[offset + 16 : offset + nlmsg_len]
+                    messages.append((nlmsg_type, payload))
+                offset += (nlmsg_len + 3) & ~3
+            if done:
+                break
+        return messages
+
+    def _parse_attrs(self, data: bytes, offset: int = 0) -> dict[int, bytes]:
+        attrs = {}
+        while offset + 4 <= len(data):
+            nla_len, nla_type = struct.unpack_from("HH", data, offset)
+            if nla_len < 4:
+                break
+            nla_type_base = nla_type & 0x7FFF
+            attr_data = data[offset + 4 : offset + nla_len]
+            attrs[nla_type_base] = attr_data
+            offset += (nla_len + 3) & ~3
+        return attrs
+
+    def _parse_nested_attrs(self, data: bytes) -> dict[int, bytes]:
+        return self._parse_attrs(data, 0)
+
+    def get_links(self) -> dict[str, LinkInfo]:
+        # Build ifinfomsg: family(1) + pad(1) + type(2) + index(4) + flags(4) + change(4) = 16 bytes
+        ifinfomsg = struct.pack("BxHiII", AddressFamily.UNSPEC, 0, 0, 0, 0)
+        # Add IFLA_EXT_MASK to request extended info but skip stats
+        ext_mask = self._pack_nlattr_u32(
+            IFLAAttr.EXT_MASK, RTEXTFilter.VF | RTEXTFilter.SKIP_STATS
+        )
+        payload = ifinfomsg + ext_mask
+        msg = self._pack_nlmsg(
+            RTMType.GETLINK, NLMsgFlags.REQUEST | NLMsgFlags.DUMP, payload
+        )
+        self._sock.send(msg)
+
+        links: dict[str, LinkInfo] = dict()
+        for msg_type, payload in self._recv_msgs():
+            if msg_type != RTMType.NEWLINK:
+                continue
+            if len(payload) < 16:
+                continue
+            # Parse ifinfomsg header
+            ifi_family, ifi_type, ifi_index, ifi_flags, ifi_change = struct.unpack_from(
+                "BxHiII", payload, 0
+            )
+            # Parse attributes after ifinfomsg (16 bytes)
+            attrs = self._parse_attrs(payload, 16)
+
+            ifname = None
+            if IFLAAttr.IFNAME in attrs:
+                ifname = (
+                    attrs[IFLAAttr.IFNAME]
+                    .rstrip(b"\x00")
+                    .decode("utf-8", errors="replace")
+                )
+            if not ifname:
+                continue
+
+            mtu = 0
+            operstate = 0
+            address = None
+            perm_address = None
+
+            if IFLAAttr.MTU in attrs:
+                mtu = struct.unpack("I", attrs[IFLAAttr.MTU][:4])[0]
+            if IFLAAttr.OPERSTATE in attrs:
+                operstate = attrs[IFLAAttr.OPERSTATE][0]
+            if IFLAAttr.ADDRESS in attrs:
+                address = attrs[IFLAAttr.ADDRESS].hex(":")
+            if IFLAAttr.PERM_ADDRESS in attrs:
+                perm_address = attrs[IFLAAttr.PERM_ADDRESS].hex(":")
+
+            links[ifname] = LinkInfo(
+                index=ifi_index,
+                flags=ifi_flags,
+                mtu=mtu,
+                operstate=operstate,
+                address=address,
+                perm_address=perm_address,
+            )
+
+        return links
+
+    def get_addresses(self) -> list[AddressInfo]:
+        # Build ifaddrmsg: family(1) + prefixlen(1) + flags(1) + scope(1) + index(4) = 8 bytes
+        ifaddrmsg = struct.pack("BBBBI", AddressFamily.UNSPEC, 0, 0, 0, 0)
+        msg = self._pack_nlmsg(
+            RTMType.GETADDR, NLMsgFlags.REQUEST | NLMsgFlags.DUMP, ifaddrmsg
+        )
+        self._sock.send(msg)
+
+        addresses: list[AddressInfo] = []
+        for msg_type, payload in self._recv_msgs():
+            if msg_type != RTMType.NEWADDR:
+                continue
+            if len(payload) < 8:
+                continue
+            # Parse ifaddrmsg header
+            ifa_family, ifa_prefixlen, ifa_flags, ifa_scope, ifa_index = (
+                struct.unpack_from("BBBBI", payload, 0)
+            )
+            # Parse attributes after ifaddrmsg (8 bytes)
+            attrs = self._parse_attrs(payload, 8)
+
+            # Get address - prefer IFA_ADDRESS, fall back to IFA_LOCAL
+            address = None
+            if IFAAttr.ADDRESS in attrs:
+                address = self._format_address(ifa_family, attrs[IFAAttr.ADDRESS])
+            elif IFAAttr.LOCAL in attrs:
+                address = self._format_address(ifa_family, attrs[IFAAttr.LOCAL])
+            if not address:
+                continue
+
+            local = None
+            broadcast = None
+            label = None
+
+            if IFAAttr.LOCAL in attrs:
+                local = self._format_address(ifa_family, attrs[IFAAttr.LOCAL])
+            if IFAAttr.BROADCAST in attrs:
+                broadcast = self._format_address(ifa_family, attrs[IFAAttr.BROADCAST])
+            if IFAAttr.LABEL in attrs:
+                label = (
+                    attrs[IFAAttr.LABEL]
+                    .rstrip(b"\x00")
+                    .decode("utf-8", errors="replace")
+                )
+
+            addresses.append(
+                AddressInfo(
+                    family=ifa_family,
+                    prefixlen=ifa_prefixlen,
+                    flags=ifa_flags,
+                    scope=ifa_scope,
+                    index=ifa_index,
+                    address=address,
+                    local=local,
+                    broadcast=broadcast,
+                    label=label,
+                )
+            )
+
+        return addresses
+
+    def _format_address(self, family: int, data: bytes) -> str | None:
+        if family == AddressFamily.INET and len(data) >= 4:
+            return socket.inet_ntop(socket.AF_INET, data[:4])
+        elif family == AddressFamily.INET6 and len(data) >= 16:
+            return socket.inet_ntop(socket.AF_INET6, data[:16])
+        return None
+
+
+def get_address_netlink() -> AddressNetlink:
+    nl = _address_ctx.get()
+    needs_reconnect = False
+    if nl is None:
+        needs_reconnect = True
+    elif nl._sock is None:
+        needs_reconnect = True
+    else:
+        try:
+            if nl._sock.fileno() == -1:
+                needs_reconnect = True
+        except OSError:
+            needs_reconnect = True
+    if needs_reconnect:
+        if nl is not None:
+            try:
+                nl.close()
+            except OSError:
+                pass
+        nl = AddressNetlink()
+        nl._connect()
+        _address_ctx.set(nl)
+    return nl
+
+
+def close_address_netlink() -> None:
+    nl = _address_ctx.get()
+    if nl is not None:
+        nl.close()
+        _address_ctx.set(None)
