31b17a7fab914bc6f06ec4237eb3736009e63ae4
caleb <yocalebo@gmail.com>
2026-01-25 18:57:52 -0500

    add set_link_mtu

--- truenas_pynetif/address/link.py
+++ truenas_pynetif/address/link.py
@@ -2,10 +2,15 @@ import socket
 import struct
 
 from truenas_pynetif.address._link_helpers import _resolve_index, _set_link_flags
-from truenas_pynetif.address.constants import AddressFamily, IFFlags, RTMType
-from truenas_pynetif.netlink._core import NLMsgFlags, pack_nlmsg, recv_msgs
+from truenas_pynetif.address.constants import AddressFamily, IFFlags, IFLAAttr, RTMType
+from truenas_pynetif.netlink._core import (
+    NLMsgFlags,
+    pack_nlattr_u32,
+    pack_nlmsg,
+    recv_msgs,
+)
 
-__all__ = ("set_link_up", "set_link_down", "delete_link")
+__all__ = ("set_link_up", "set_link_down", "set_link_mtu", "delete_link")
 
 
 def set_link_up(
@@ -20,6 +25,29 @@ def set_link_down(
 ) -> None:
     """Bring a network interface down."""
     _set_link_flags(sock, 0, IFFlags.UP, name=name, index=index)
+
+
+def set_link_mtu(
+    sock: socket.socket,
+    mtu: int,
+    name: str | None = None,
+    *,
+    index: int | None = None,
+) -> None:
+    """Set the MTU of a network interface.
+
+    Args:
+        sock: Netlink socket from netlink_route()
+        mtu: Maximum transmission unit size in bytes
+        name: Interface name (mutually exclusive with index)
+        index: Interface index (mutually exclusive with name)
+    """
+    index = _resolve_index(name, index)
+    ifinfomsg = struct.pack("BxHiII", AddressFamily.UNSPEC, 0, index, 0, 0)
+    attrs = pack_nlattr_u32(IFLAAttr.MTU, mtu)
+    msg = pack_nlmsg(RTMType.NEWLINK, NLMsgFlags.REQUEST | NLMsgFlags.ACK, ifinfomsg + attrs)
+    sock.send(msg)
+    recv_msgs(sock)
 
 
 def delete_link(
